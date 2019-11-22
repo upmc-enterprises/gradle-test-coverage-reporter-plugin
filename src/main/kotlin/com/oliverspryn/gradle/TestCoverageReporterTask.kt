c0ec10823febe00e65c0a5d53ac09af7a23769c3
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-11-22 10:21:26 -0500

    Extracting and reporting metrics now

--- src/main/kotlin/com/oliverspryn/gradle/TestCoverageReporterTask.kt
+++ src/main/kotlin/com/oliverspryn/gradle/TestCoverageReporterTask.kt
@@ -10,24 +10,57 @@ open class TestCoverageReporterTask @Inject constructor(
     private val extension: TestCoverageReporterExtension
 ) : DefaultTask() {
 
+    private val coverageTypeCache: HashMap<String, Float> = hashMapOf()
+
     @TaskAction
     fun extractFromReport() {
         if (!extension.enabled) return
 
+        val includedCoverageTypes = extension.coverageTypes.map {
+            it.toLowerCase()
+        }
+
         for (module in extension.modules.modules) {
             val document = SAXReader().read(File(module.reportPath))
-            val metrics = document.rootElement.elements("counter")
+            val coverageTypes = document.rootElement.elements("counter")
 
-            for (metric in metrics) {
-                val covered = metric.attribute("covered").value.toFloat()
-                val metricName = metric.attribute("type").value.toLowerCase().capitalize()
-                val missed = metric.attribute("missed").value.toFloat()
+            for (coverageType in coverageTypes) {
+                val coverageTypeName = coverageType.attribute("type").value.toLowerCase().capitalize()
+                if (!includedCoverageTypes.contains(coverageTypeName.toLowerCase())) continue
+
+                val covered = coverageType.attribute("covered").value.toFloat()
+                val missed = coverageType.attribute("missed").value.toFloat()
                 val percentage = (covered / (covered + missed)) * 100f
 
-                println("${module.name} $metricName - ${"%.2f".format(percentage)}%")
+                addToCache(coverageTypeName, percentage)
+                println("${module.name} $coverageTypeName - ${"%.2f".format(percentage)}%")
             }
 
             println()
+        }
+
+        if (extension.modules.modules.count() <= 1) return
+        val totalModules = extension.modules.modules.count().toFloat()
+
+        for (key in coverageTypeCache.keys) {
+            coverageTypeCache[key]?.let {
+                val percentage = it / totalModules
+                println("Summary ${key.capitalize()} - ${"%.2f".format(percentage)}%")
+            }
+        }
+
+        println()
+    }
+
+    private fun addToCache(coverageTypeName: String, percentage: Float) {
+        val name = coverageTypeName.toLowerCase()
+
+        if (!coverageTypeCache.containsKey(name)) {
+            coverageTypeCache[name] = 0f
+        }
+
+        coverageTypeCache[name]?.let {
+            coverageTypeCache[name] = it + percentage
         }
     }
 }
